fe1754a2c2ab2db6daf54d77664b7a3cf39b6e15
AndrewGls <mrglushko@gmail.com>
2017-07-30 17:11:21 +0200

    added quizzes

---
+++ QuinticPolynomialSolver/main.cpp
@@ -0,0 +1,150 @@
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <vector>
+
+#include "Dense"
+
+using namespace std;
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+
+// TODO - complete this function
+vector<double> JMT(vector< double> start, vector <double> end, double T)
+{
+	/*
+	Calculate the Jerk Minimizing Trajectory that connects the initial state
+	to the final state in time T.
+
+	INPUTS
+
+	start - the vehicles start location given as a length three array
+	corresponding to initial values of [s, s_dot, s_double_dot]
+
+	end   - the desired end state for vehicle. Like "start" this is a
+	length three array.
+
+	T     - The duration, in seconds, over which this maneuver should occur.
+
+	OUTPUT
+	an array of length 6, each value corresponding to a coefficent in the polynomial
+	s(t) = a_0 + a_1 * t + a_2 * t**2 + a_3 * t**3 + a_4 * t**4 + a_5 * t**5
+
+	EXAMPLE
+
+	> JMT( [0, 10, 0], [10, 10, 0], 1)
+	[0.0, 10.0, 0.0, 0.0, 0.0, 0.0]
+	*/
+
+	double T2 = T*T;
+	double T3 = T2*T;
+	double T4 = T3*T;
+
+	double Si = start[0];
+	double Si_dot = start[1];
+	double Si_double_dot = start[2];
+
+	double Sf = end[0];
+	double Sf_dot = end[1];
+	double Sf_double_dot = end[2];
+
+	MatrixXd A(3,3);
+	VectorXd b(3);
+	VectorXd x(3);
+
+	A << T3, T4, T4*T, 
+		 3*T2, 4*T3, 5*T4,
+		 6*T, 12*T2, 20*T3;
+
+	b << Sf - (Si + Si_dot*T + Si_double_dot*T2 / 2.),
+		 Sf_dot - (Si_dot + Si_double_dot*T),
+		 Sf_double_dot - Si_double_dot;
+
+	x = A.colPivHouseholderQr().solve(b);
+
+	std::vector<double> result;
+	result.resize(6);
+
+	result[0] = Si;
+	result[1] = Si_dot;
+	result[2] = Si_double_dot/2.;
+	result[3] = x[0];
+	result[4] = x[1];
+	result[5] = x[2];
+
+	return result;
+}
+
+bool close_enough(vector< double > poly, vector<double> target_poly, double eps = 0.01) {
+
+
+	if (poly.size() != target_poly.size())
+	{
+		cout << "your solution didn't have the correct number of terms" << endl;
+		return false;
+	}
+	for (int i = 0; i < poly.size(); i++)
+	{
+		double diff = poly[i] - target_poly[i];
+		if (abs(diff) > eps)
+		{
+			cout << "at least one of your terms differed from target by more than " << eps << endl;
+			return false;
+		}
+
+	}
+	return true;
+}
+
+struct test_case {
+
+	vector<double> start;
+	vector<double> end;
+	double T;
+};
+
+vector< vector<double> > answers = { { 0.0, 10.0, 0.0, 0.0, 0.0, 0.0 },{ 0.0,10.0,0.0,0.0,-0.625,0.3125 },{ 5.0,10.0,1.0,-3.0,0.64,-0.0432 } };
+
+int main() {
+
+	//create test cases
+
+	vector< test_case > tc;
+
+	test_case tc1;
+	tc1.start = { 0,10,0 };
+	tc1.end = { 10,10,0 };
+	tc1.T = 1;
+	tc.push_back(tc1);
+
+	test_case tc2;
+	tc2.start = { 0,10,0 };
+	tc2.end = { 20,15,20 };
+	tc2.T = 2;
+	tc.push_back(tc2);
+
+	test_case tc3;
+	tc3.start = { 5,10,2 };
+	tc3.end = { -30,-20,-4 };
+	tc3.T = 5;
+	tc.push_back(tc3);
+
+	bool total_correct = true;
+	for (int i = 0; i < tc.size(); i++)
+	{
+		vector< double > jmt = JMT(tc[i].start, tc[i].end, tc[i].T);
+		bool correct = close_enough(jmt, answers[i]);
+		total_correct &= correct;
+
+	}
+	if (!total_correct)
+	{
+		cout << "Try again!" << endl;
+	}
+	else
+	{
+		cout << "Nice work!" << endl;
+	}
+
+	return 0;
+}
